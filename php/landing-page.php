9cb8dc729fd3db334ea6fa1d23f14e77f52a88dc
Hosea Noabeb Chris <122771226+Hozei@users.noreply.github.com>
2024-09-03 15:04:31 -0700

    landing-page Update

--- php/landing-page.php
+++ php/landing-page.php
@@ -20,9 +20,9 @@
             <a href="#">Heavenly Tomb</a>
         </div>
         <ul class="navbar-menu">
-            <li><a href="#">Home</a></li>
+            <li><a href="./index.php">Home</a></li>
             <li><a href="#features">Features</a></li>
-            <li><a href="#">Services</a></li>
+            <li><a href="./serviceProvidersPage.php">Services</a></li>
             <li><a href="#">Contact</a></li>
         </ul>
     </div>
